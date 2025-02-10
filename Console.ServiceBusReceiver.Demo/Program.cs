b84d59954253f199ffb0d24371165cff62fb3d27
Nicolás Snider <nicolas.snider@outlook.com>
2025-02-10 11:22:25 -0300

    Add Service Bus receiver demo targeting .NET 8.0
    Created/modified project file to target .NET 8.0, including settings for implicit usings and nullable reference types. Added reference to `Azure.Messaging.ServiceBus` package version 7.18.3.
    
    Updated `Program.cs` to initialize `ServiceBusClient` and `ServiceBusProcessor` for handling messages from the Service Bus queue. Included asynchronous message handler and error handler. Properly disposed of processor and client.

---
+++ Console.ServiceBusReceiver.Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "<connection-string>";
+const string queueName = "<queue-name>";
+
+ServiceBusClient client = new ServiceBusClient(serviceBusConnectionString);
+ServiceBusProcessor processor = default;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processErrorEventArgs)
+{
+    Console.WriteLine($"An error occurred. Error: {processErrorEventArgs.Exception.Message}");
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(serviceBusConnectionString);
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end processing");
+    Console.ReadKey();
+
+    await processor.StopProcessingAsync();
+}
+catch (Exception ex)
+{
+
+    throw;
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
