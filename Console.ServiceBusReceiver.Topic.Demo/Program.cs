5be16c6d28dd7095cc561f540439fc7eee5bedea
Nicolás Snider <nicolas.snider@outlook.com>
2025-02-10 13:29:37 -0300

    Add new Service Bus topic demo project
    Added new .NET 8.0 project `Console.ServiceBusReceiver.Topic.Demo` with implicit usings and nullable reference types. Included `Azure.Messaging.ServiceBus` package version `7.18.3`. Updated `Program.cs` to send messages to an Azure Service Bus topic, including connection setup, message creation, and exception handling. Updated `azure-app-nico.sln` to include the new project with appropriate configurations and nesting.

---
+++ Console.ServiceBusReceiver.Topic.Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "<connection-string>";
+const string topicName = "<topic-name>";
+const int maxNumberOfMessages = 3;
+
+ServiceBusClient client = new ServiceBusClient(serviceBusConnectionString);
+ServiceBusSender sender = client.CreateSender(topicName);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i < maxNumberOfMessages; i++)
+{
+    if (!batch.TryAddMessage(new ServiceBusMessage($"This is message {i}")))
+    {
+        Console.WriteLine($"Message {i} not addeed to the list");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(batch);
+    Console.WriteLine("Messages sent successfully");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+    throw;
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
